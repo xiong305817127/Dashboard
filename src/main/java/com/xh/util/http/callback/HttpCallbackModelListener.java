f1996242e0474c368fa70d542d5cc47a6f7e59ea
xiong305817127 <305817127@qq.com>
2017-11-01 20:23:08 +0800

    Update HttpCallbackModelListener.java

--- src/main/java/com/xh/util/http/callback/HttpCallbackModelListener.java
+++ src/main/java/com/xh/util/http/callback/HttpCallbackModelListener.java
@@ -1,12 +1,13 @@
 package com.xh.util.http.callback;
 
+import java.util.List;
 
 /**
  * HttpURLConnection网络请求返回监听器
  */
 public interface  HttpCallbackModelListener<T> {
     // 网络请求成功
-    void onFinish(T response);
+    void onFinish(List<T> response);
 
     // 网络请求失败
     void onError(Exception e);
